e88f3f6606911403719b06fb7e1bae36e96bf19f
BIENAIMEE Marie Reine <m.bienaimee@alustudent.com>
2023-11-08 21:50:43 +0000

    api

--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -3,51 +3,52 @@
 import json
 import requests
 
+def count_words(subreddit, word_list, after=None, counts=None):
+    if counts is None:
+        counts = {}  # Initialize the counts dictionary
 
-def count_words(subreddit, word_list, after="", count=[]):
-    """ prints a sorted count of given keywords """
+    # Base case: if word_list is empty, we're done
+    if not word_list:
+        sorted_counts = sorted(counts.items(), key=lambda x: (-x[1], x[0]))
+        for word, count in sorted_counts:
+            print(f"{word.lower()}: {count}")
+        return
 
-    if after == "":
-        count = [0] * len(word_list)
+    if after is None:
+        params = {'limit': 100}
+    else:
+        params = {'limit': 100, 'after': after}
 
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    request = requests.get(url,
-                           params={'after': after},
-                           allow_redirects=False,
-                           headers={'User-Agent': 'Mozilla/5.0'})
+    response = requests.get(f'https://www.reddit.com/r/{subreddit}/hot.json', headers={'User-agent': 'Mozilla/5.0'}, params=params)
 
-    if request.status_code == 200:
-        data = request.json()
+    if response.status_code != 200:
+        return  # Invalid subreddit or other issue
 
-        for topic in (data['data']['children']):
-            for word in topic['data']['title'].split():
-                for i in range(len(word_list)):
-                    if word_list[i].lower() == word.lower():
-                        count[i] += 1
+    data = response.json().get('data', {})
+    children = data.get('children', [])
 
-        after = data['data']['after']
-        if after is None:
-            save = []
-            for i in range(len(word_list)):
-                for j in range(i + 1, len(word_list)):
-                    if word_list[i].lower() == word_list[j].lower():
-                        save.append(j)
-                        count[i] += count[j]
+    for post in children:
+        title = post['data']['title']
+        for word in word_list:
+            # Remove punctuation and convert to lowercase for accurate counting
+            clean_title = title.lower().replace('.', ' ').replace('!', ' ').replace('_', ' ')
+            word_list_copy = list(word_list)  # Create a copy to avoid modifying the original list
+            if word in clean_title.split():
+                counts[word] = counts.get(word, 0) + 1
+                word_list_copy.remove(word)  # Remove the counted word from the list
 
-            for i in range(len(word_list)):
-                for j in range(i, len(word_list)):
-                    if (count[j] > count[i] or
-                            (word_list[i] > word_list[j] and
-                             count[j] == count[i])):
-                        aux = count[i]
-                        count[i] = count[j]
-                        count[j] = aux
-                        aux = word_list[i]
-                        word_list[i] = word_list[j]
-                        word_list[j] = aux
+        if not word_list_copy:  # All keywords found, continue with the next word
+            continue
 
-            for i in range(len(word_list)):
-                if (count[i] > 0) and i not in save:
-                    print("{}: {}".format(word_list[i].lower(), count[i]))
-        else:
-            count_words(subreddit, word_list, after, count)
+    if data.get('after'):
+        count_words(subreddit, word_list_copy, after=data['after'], counts=counts)
+
+if __name__ == '__main__':
+    import sys
+
+    if len(sys.argv) < 3:
+        print("Usage: {} <subreddit> <list of keywords>".format(sys.argv[0]))
+        print("Ex: {} programming 'python java javascript'".format(sys.argv[0]))
+    else:
+        count_words(sys.argv[1], [x for x in sys.argv[2].split()])
+
